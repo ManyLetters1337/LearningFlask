85112f952fdb8b6c0dc75861d7a52e54112dffdc
ManyLetters1337 <VS20132013@gmail.com>
2019-11-13 09:30:04 +0300

    Add Registration

--- models.py
+++ models.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin 
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Many_Letters:VitaLol1337@localhost/testDB'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.query(User).get(user_id)
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
