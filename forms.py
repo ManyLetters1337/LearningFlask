7c16c2c527d3ca606d97e41c97e65f7c20666af1
ManyLetters1337 <VS20132013@gmail.com>
2019-11-13 09:28:00 +0300

    Create refistration form

--- forms.py
+++ forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField,  BooleanField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
 	username = StringField("Username", validators=[DataRequired()])
 	password = PasswordField("Password", validators=[DataRequired()])
 	remember = BooleanField("Remember me")
 	submit = SubmitField()
+
+
+class RegistrationForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired()])
+	email = StringField("Email", validators=[Email()])
+	password = PasswordField("Password", validators=[DataRequired()])
+	password_repeat = PasswordField("Password Repeat", validators=[DataRequired()])
+	submit = SubmitField()
+
+	def check_password(self):
+		return self.password == self.password_repeat
+
