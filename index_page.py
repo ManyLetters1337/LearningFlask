080f4b5190ccc76a15d19ff928ca750778f0deb1
ManyLetters1337 <VS20132013@gmail.com>
2019-11-13 15:12:44 +0300

    Add Function for Add Note

--- index_page.py
+++ index_page.py
@@ -2,7 +2,8 @@ from flask import Flask, flash, url_for, session, request, render_template, redi
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 from models import User, Note
-from forms import LoginForm, RegistrationForm
+from forms import LoginForm, RegistrationForm, AddNoteForm
+from datetime import datetime
 
 
 import os
@@ -49,7 +50,6 @@ def registration():
 			if form.check_password:
 				user = User(username = form.username.data, email = form.email.data)
 				user.set_password(form.password.data)
-				print(db.session.query(User).filter(User.username == user.username))
 				db.session.add(user)
 				db.session.commit()
 				session['user_id'] = db.session.query(User).filter(User.username == user.username).first().id
@@ -67,6 +67,16 @@ def logout():
     flash("You have been logged out.")
     return redirect(url_for('login'))
 
+@app.route('/add_note', methods = ['POST', 'GET'])
+def add_note():
+	form = AddNoteForm()
+	if form.validate_on_submit():
+		note = Note(title = form.title.data, description = form.description.data, status = False)
+		note.set_created()
+		db.session.add(note)
+		db.session.commit()
+		return redirect(url_for('index_page'))
+	return render_template('add_note.html', form=form)	
 
 if __name__ == '__main__':
 	app.run();
