10a921df02390b7f2177f03941b9a76a7e8ea9a1
ManyLetters1337 <VS20132013@gmail.com>
2019-11-13 09:28:32 +0300

    Create registration function

--- index_page.py
+++ index_page.py
@@ -1,25 +1,20 @@
 from flask import Flask, flash, url_for, session, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, login_required, login_user, current_user
+from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 from models import User, Note
-from forms import LoginForm
+from forms import LoginForm, RegistrationForm
 
 
 import os
 
 app = Flask(__name__)
-app.debug = False
+app.debug = True
 app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Many_Letters:VitaLol1337@localhost/testDB'
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    return db.session.query(User).get(user_id)
 
 @app.route('/')
 def index_page():
@@ -28,6 +23,12 @@ def index_page():
 	else:
 		return render_template("index_page.html") 
 
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.query(User).get(user_id)
+
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
 	form = LoginForm()
@@ -35,16 +36,36 @@ def login():
 		user = db.session.query(User).filter(User.username == form.username.data).first()
 		if user and user.check_password(form.password.data):
 			login_user(user, remember=form.remember.data)
-			return redirect(url_for('admin'))
+			return redirect(url_for('index_page'))
+		flash("Invalid username/password", 'error')
 		return redirect(url_for('login'))
 	return render_template('login.html', form=form)
 
+@app.route('/registration', methods = ['POST', 'GET'])
+def registration():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		if not db.session.query(User).filter(User.username == form.username.data).first():
+			if form.check_password:
+				user = User(username = form.username.data, email = form.email.data)
+				user.set_password(form.password.data)
+				print(db.session.query(User).filter(User.username == user.username))
+				db.session.add(user)
+				db.session.commit()
+				session['user_id'] = db.session.query(User).filter(User.username == user.username).first().id
+				return redirect(url_for('login'))
+			flash("Invalid password repeat", 'error')
+		flash("User already exist", 'error')
+		return redirect(url_for('registration'))
+	return render_template('registration.html', form=form)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-	session.pop('username', None)
-	return redirect(url_for('index_page'))
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
