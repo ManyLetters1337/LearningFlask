79132ea9b1f3e2d7ca4baa3d58a4808461d5cc8d
ManyLetters1337 <VS20132013@gmail.com>
2019-11-13 08:11:56 +0300

    Create Models

---
+++ models.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin 
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Many_Letters:VitaLol1337@localhost/testDB'
+db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(70), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(100), nullable=False)
+    notes = db.relationship('Note', backref='user')
+
+    def set_password(self, password):
+    	self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+    	return check_password_hash(self.password_hash, password)
+
+
+class Note(db.Model):
+	__tablename__ = 'notes'
+	id = db.Column(db.Integer(), primary_key = True)
+	user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+	title = db.Column(db.String(255), nullable = False)
+	description = db.Column(db.String(511), nullable = True)
+	created_on = db.Column(db.DateTime(), default=datetime.utcnow)
