4b02e389220a130cae32bbecc22406f6fa76db12
ManyLetters1337 <VS20132013@gmail.com>
2019-11-19 09:16:56 +0300

    Add methods for Deleting and Changing Notes

--- notes/services.py
+++ notes/services.py
@@ -3,24 +3,20 @@ Database interaction methods for a Note class
 """
 from database.core import db
 from database.base_services import BaseDBService
-from typing import TYPE_CHECKING
 from .models import Note
-
-
-# if TYPE_CHECKING:
 
 
 class NoteDBService(BaseDBService):
     model = Note
 
-    def get_notes(self) -> 'Note':
+    def get_notes(self) -> Note:
         """
         Get notes
         :return: List of all notes
         """
         return db.session.query(Note).all()
 
-    def get_notes_for_user(self, user_id: int) -> 'Note':
+    def get_notes_for_user(self, user_id: int) -> Note:
         """
         Get note for current user
         :param user_id:
@@ -28,17 +24,16 @@ class NoteDBService(BaseDBService):
         """
         return db.session.query(Note).filter(Note.user_id == user_id).order_by(db.desc(Note.created_on)).all()
 
-    # def get_id_by_title_and_description(self, title: str, description: str) -> db.Model:
-    #     """
-    #     Get note by title and description
-    #     :param title:
-    #     :param description:
-    #     :return:
-    #     """
-    #     print(db.session.query(Note).filter(Note.title == title and Note.description == description).first().id)
-    #     return db.session.query(Note).filter(Note.title == title and Note.description == description).first()
+    def get_id_by_title_and_description(self, title: str, description: str) -> id:
+        """
+        Get note by title and description
+        :param title:
+        :param description:
+        :return:
+        """
+        return db.session.query(Note).filter(Note.title == title and Note.description == description).first()
 
-    def create(self, title: str, description: str, user_id: int) -> 'Note':
+    def create(self, title: str, description: str, user_id: int) -> Note:
         """
         Create note instance
         :param title:
@@ -46,14 +41,36 @@ class NoteDBService(BaseDBService):
         :param user_id:
         :return: Note instance
         """
-        note = super(NoteDBService, self).create(title=title, description=description, status=False)
+        note: Note = super(NoteDBService, self).create(title=title, description=description, status=False)
         note.set_user(user_id)
 
         db.session.commit()  # уточнить
 
         return note
 
-    # def delete_note(self, title: str, description: str) -> db.Model:
-    #     note = self.get_id_by_title_and_description(title, description)
-    #     db.session.delete(note)
-    #     db.session.commit()
+    def delete_note(self, uuid_: str) -> db.Model:
+        """
+        Delete Note
+        :param uuid_:
+        :return:   ???
+        """
+        note: Note = self.get_by_uuid(uuid_)
+
+        db.session.delete(note)
+        db.session.commit()
+
+    def change_note(self, uuid_: str, title: str, description: str) -> Note:
+        """
+        Change data in Note
+        :param uuid_:
+        :param title:
+        :param description:
+        :return: Note
+        """
+        note: Note = self.get_by_uuid(uuid_)
+        note.title = title
+        note.description = description
+
+        db.session.commit()
+
+        return note
