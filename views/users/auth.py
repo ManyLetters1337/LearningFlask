afcf2df8d52256e3e26537ff2074dd11e2006f94
ManyLetters1337 <VS20132013@gmail.com>
2019-11-21 16:11:23 +0300

    Add redirect

--- views/users/auth.py
+++ views/users/auth.py
@@ -2,8 +2,7 @@
 Authentication views for User Class
 """
 from typing import TYPE_CHECKING
-
-from flask import flash, url_for, render_template, redirect
+from flask import flash, url_for, render_template, redirect, request
 from app import app
 from form.forms import LoginForm, RegistrationForm
 from database.service_registry import services
@@ -37,7 +36,7 @@ def login():
     if form.validate_on_submit():
         login_user(services.users.get_by_id(services.users.get_id_by_name(form.username.data)),
                    remember=form.remember.data)
-        return redirect(url_for('notes_page'))
+        return redirect(request.args.get('next') or url_for('notes_page'))
 
     return render_template('users/auth/login.html', form=form)
 
@@ -52,7 +51,7 @@ def registration():
 
     if form.validate_on_submit():
         user: 'User' = services.users.create(form.username.data, form.email.data, form.password.data)
-        return redirect(url_for('notes_page'))
+        return redirect(request.args.get('next') or url_for('notes_page'))
 
     return render_template('users/auth/registration.html', form=form)
 
