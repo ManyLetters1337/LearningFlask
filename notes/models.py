077646397d648e4737c9a471a1c14345a837fb46
ManyLetters1337 <VS20132013@gmail.com>
2019-11-21 11:44:17 +0300

    Add project_id in model

--- notes/models.py
+++ notes/models.py
@@ -3,6 +3,7 @@ Note Class
 """
 from database.core import db
 from datetime import datetime
+import uuid
 
 
 class Note(db.Model):
@@ -13,10 +14,11 @@ class Note(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     uuid = db.Column(db.String(), nullable=False, unique=True)
     user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    project_id = db.Column(db.Integer(), db.ForeignKey('projects.id'))
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(500), nullable=True)
-    status = db.Column(db.Boolean())
-    created_on = db.Column(db.DateTime(), default=datetime.now)
+    status = db.Column(db.String(), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.now, onupdate=datetime.now)
 
     def set_uuid(self, uuid_):
         """
@@ -24,6 +26,14 @@ class Note(db.Model):
         :param uuid_:
         """
         self.uuid = uuid_
+
+    def set_project(self, id_):
+        """
+        Set project in note
+        :param id_:
+        :return:
+        """
+        self.project_id = id_
 
     def set_user(self, id_):
         """
