ceadc77e6d60d388e15d4cd8ee360f5313e4ed79
ManyLetters1337 <VS20132013@gmail.com>
2019-11-21 13:55:14 +0300

    Add new method Get Notes For Form

--- projects/services.py
+++ projects/services.py
@@ -1,9 +1,9 @@
 """
-Database interaction methods for a Note class
+Database interaction methods for a Project class
 """
 from database.core import db
 from database.base_services import BaseDBService
-# from notes.services import NoteDBService
+from notes.models import Note
 from .models import Project
 from flask import session
 import uuid
@@ -14,11 +14,11 @@ class ProjectDBService(BaseDBService):
 
     def create(self, title: str, description: str, user_id: int) -> Project:
         """
-        Create note instance
+        Create project instance
         :param title:
         :param description:
         :param user_id:
-        :return: Note instance
+        :return:
         """
         project: Project = super(ProjectDBService, self).new(title=title, description=description)
         project.set_user(user_id)
@@ -30,7 +30,7 @@ class ProjectDBService(BaseDBService):
 
     def delete_project(self, uuid_: str):
         """
-        Delete Note
+        Delete project instance
         :param uuid_:
         :return:
         """
@@ -43,11 +43,11 @@ class ProjectDBService(BaseDBService):
 
     def change_project(self, uuid_: str, title: str, description: str) -> Project:
         """
-        Change data in Note
+        Change project data
         :param uuid_:
         :param title:
         :param description:
-        :return: Note
+        :return:
         """
         project: Project = self.get_by_uuid(uuid_)
         project.title = title
@@ -58,17 +58,33 @@ class ProjectDBService(BaseDBService):
 
         return project
 
+    def get_notes_for_project(self, id_):
+        """
+        Get note for current project
+        :param id_:
+        :return:
+        """
+        return db.session.query(Note).filter(Note.project_id == id_).all()
+
     def get_projects(self) -> Project:
         """
-        Get notes
-        :return: List of all notes
+        Get projects
+        :return: List of all project
         """
         return db.session.query(Project).all()
 
     def get_projects_for_user(self, user_id: int) -> Project:
         """
-        Get note for current user
+        Get projects for current user
         :param user_id:
-        :return: List of notes for user
+        :return: List of projects for user
         """
         return db.session.query(Project).filter(Project.user_id == user_id).order_by(db.desc(Project.created_on)).all()
+
+    def get_projects_for_form(self, user_id: int) -> Project:
+        """
+        Get projects for form
+        :param user_id:
+        :return: List of projects for form
+        """
+        return db.session.query(Project).filter(Project.user_id == user_id).all()
