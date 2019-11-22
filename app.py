8bdb0d2be09bdc6b219de71fefe5e2c745e69a81
ManyLetters1337 <VS20132013@gmail.com>
2019-11-22 09:26:35 +0300

    Add page_size for pagination

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from flask import Flask
 logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 app = Flask(__name__)
-app.template_folder = "templates"
 app.debug = True
+page_size = 2
 app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://manyletters:12345678*Aa@localhost/base'
 
