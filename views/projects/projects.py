e3035fceb65db8f19b13bd179939737a61cf2f4c
ManyLetters1337 <VS20132013@gmail.com>
2019-11-22 09:26:59 +0300

    Refactor for pagination

--- views/projects/projects.py
+++ views/projects/projects.py
@@ -2,6 +2,7 @@
 Views for Project Class
 """
 from app import app
+from app import page_size
 from flask import url_for, render_template, redirect, session, request
 from form.forms import ProjectForm
 from database.service_registry import services
@@ -13,11 +14,13 @@ if TYPE_CHECKING:
 
 
 @app.route('/projects', methods=['GET'])
+@app.route('/projects/<int:page>', methods=['GET'])
 @login_required
-def projects_page():
+def projects_page(page=1):
     if 'user_id' in session:
         return render_template("projects/projects.html",
-                               projects=services.projects.get_projects_for_user(session["user_id"]))
+                               projects=services.projects.apply_pagination(services.projects.get_projects_for_user(
+                                   session["user_id"]), page, page_size))
     else:
         return render_template("projects/projects.html")
 
@@ -36,12 +39,13 @@ def add_project():
 
 
 @app.route('/projects/<uuid>/', methods=['POST', 'GET'])
+@app.route('/projects/<uuid>/<int:page>', methods=['POST', 'GET'])
 @login_required
-def project(uuid: str):
+def project(uuid: str, page=1):
     form: ProjectForm = ProjectForm()
     project_ = services.projects.get_by_uuid(uuid)
     form.description.data = project_.description
-    notes = services.projects.get_notes_for_project(project_.id)
+    notes = services.notes.apply_pagination(services.projects.get_notes_for_project(project_.id), page, page_size)
 
     if form.validate_on_submit():
         if request.form['button'] == 'Delete':
