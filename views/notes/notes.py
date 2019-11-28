79ced2144e0991f64eaf5596e5a96b2924d26bf1
ManyLetters1337 <VS20132013@gmail.com>
2019-11-28 15:28:21 +0300

    Add Blueprint

--- views/notes/notes.py
+++ views/notes/notes.py
@@ -1,54 +1,70 @@
 """
 Views for Note Class
 """
-from app import app
 from app import page_size
 from flask import url_for, render_template, redirect, session, request, Blueprint
-from form.forms import NoteForm
+from form.forms import create_note_form
 from database.service_registry import services
 from flask_login import login_required
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from notes.models import Note
+    from form.forms import NoteForm
+
+notes = Blueprint('notes', __name__, template_folder='templates')
 
 
-@app.route('/', methods=['GET'])
-@app.route('/note', methods=['GET'])
-@app.route('/note/<int:page>', methods=['GET'])
+@notes.route('/', methods=['GET'])
 @login_required
-def notes_page(page=1):
+def notes_page():
     """
     Page with notes
     :return: Page with notes
     """
-    if 'user_id' in session:
-        return render_template("notes/notes.html", notes=services.notes.apply_pagination(
-            services.notes.get_notes_for_user(session["user_id"]), page, page_size))
-    else:
-        return render_template("notes/notes.html")
+    page = int(request.args.get('page', default=1))
+    notes_for_user = services.notes.get_notes_for_user(session["user_id"])
+
+    return render_template(
+        "/notes.html",
+        notes=services.notes.apply_pagination(notes_for_user, page, page_size)
+    )
 
 
-@app.route('/add_note', methods=['POST', 'GET'])
+@notes.route('/add_note', methods=['GET'])
 @login_required
 def add_note():
     """
-    Page for Add Note
+    Get method for Add Note
     :return: Page with Add Note form or Page with Notes
+    Page for Add Note
     """
-    form: NoteForm = NoteForm()
-    projects = services.projects.get_projects_for_form(session['user_id'])
-    form.project.choices = [(project.id, project.title) for project in projects]
+    form: 'NoteForm' = create_note_form()
 
-    if form.validate_on_submit():
-        note_: 'Note' = services.notes.create(form.title.data, form.description.data,
-                                              session['user_id'], form.status.data, form.project.data)
-        return redirect(url_for('notes_page'))
-
-    return render_template('notes/add_note.html', form=form)
+    return render_template('add_note.html', form=form)
 
 
-@app.route('/note/<uuid>/', methods=['POST', 'GET'])
+@notes.route('/add_note', methods=['POST'])
+@login_required
+def add_note_post():
+    """
+    Post method for Add Note
+    :return:
+    """
+    form: 'NoteForm' = create_note_form()
+
+    note_: 'Note' = services.notes.create(
+        session['user_id'],
+        title=form.title.data,
+        description=form.description.data,
+        status=form.status.data,
+        project_id=form.project.data
+    )
+    return redirect(url_for('notes.notes_page'))
+
+
+@notes.route('/<uuid>', methods=['GET'])
 @login_required
 def note(uuid: str):
     """
@@ -56,22 +72,30 @@ def note(uuid: str):
     :param uuid:
     :return:
     """
-    form: NoteForm = NoteForm()
-    note_instance = services.notes.get_by_uuid(uuid)
-    projects = services.projects.get_projects_for_form(session['user_id'])
+    note_instance: 'Note' = services.notes.get_by_uuid(uuid)
+    form = create_note_form(
+        description=note_instance.description,
+        status=note_instance.status,
+        project=note_instance.project_id
+    )
 
-    form.description.data = note_instance.description
-    form.status.data = note_instance.status
-    form.project.choices = [(project.id, project.title) for project in projects]
-    form.project.data = note_instance.project_id
+    return render_template('note.html', note=note_instance, form=form)
 
-    if form.validate_on_submit():
-        if request.form['button'] == 'Delete':
-            services.notes.delete_note(uuid)
-        elif request.form['button'] == 'Change':
-            note_: 'Note' = services.notes.change_note(uuid, form.title.data,
-                                                       request.form['description'], request.form['status'],
-                                                       request.form['project'])
-        return redirect(url_for('notes_page'))
 
-    return render_template('notes/note.html', note=note_instance, form=form)
+@notes.route('/<uuid>', methods=['POST'])
+@login_required
+def note_post(uuid: str):
+    """
+    Post method for Note Page
+    :param uuid:
+    :return:
+    """
+    form: 'NoteForm' = create_note_form()
+
+    if request.form['button'] == 'Delete':
+        services.notes.delete_note(uuid)
+    elif request.form['button'] == 'Change':
+        note_: 'Note' = services.notes.change_note(uuid, title=form.title.data, description=request.form['description'],
+                                                   status=request.form['status'], project=request.form['project'])
+
+    return redirect(url_for('notes.notes_page'))
