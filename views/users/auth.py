718d1180eedea9c27eea19f78f4aa301336515b0
ManyLetters1337 <VS20132013@gmail.com>
2019-11-28 15:28:38 +0300

    Add Blueprint

--- views/users/auth.py
+++ views/users/auth.py
@@ -2,7 +2,7 @@
 Authentication views for User Class
 """
 from typing import TYPE_CHECKING
-from flask import flash, url_for, render_template, redirect, request
+from flask import flash, url_for, render_template, redirect, request, Blueprint
 from app import app
 from form.forms import LoginForm, RegistrationForm
 from database.service_registry import services
@@ -12,8 +12,9 @@ if TYPE_CHECKING:
     from users.models import User
 
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'auth.login'
 
+auth = Blueprint('auth', __name__, template_folder='templates')
 
 @login_manager.user_loader
 def load_user(user_id: int) -> 'User':
@@ -25,7 +26,7 @@ def load_user(user_id: int) -> 'User':
     return services.users.get_by_id(user_id)
 
 
-@app.route('/login', methods=['POST', 'GET'])
+@auth.route('/login', methods=['POST', 'GET'])
 def login():
     """
     User login
@@ -36,12 +37,12 @@ def login():
     if form.validate_on_submit():
         login_user(services.users.get_by_id(services.users.get_id_by_name(form.username.data)),
                    remember=form.remember.data)
-        return redirect(request.args.get('next') or url_for('notes_page'))
+        return redirect(request.args.get('next') or url_for('notes.notes_page'))
 
-    return render_template('users/auth/login.html', form=form)
+    return render_template('/auth/login.html', form=form)
 
 
-@app.route('/registration', methods=['POST', 'GET'])
+@auth.route('/registration', methods=['POST', 'GET'])
 def registration():
     """
     Registered user
@@ -51,12 +52,12 @@ def registration():
 
     if form.validate_on_submit():
         user: 'User' = services.users.create(form.username.data, form.email.data, form.password.data)
-        return redirect(request.args.get('next') or url_for('notes_page'))
+        return redirect(request.args.get('next') or url_for('notes.notes_page'))
 
-    return render_template('users/auth/registration.html', form=form)
+    return render_template('/auth/registration.html', form=form)
 
 
-@app.route('/logout')
+@auth.route('/logout')
 @login_required
 def logout():
     """
@@ -66,4 +67,4 @@ def logout():
     logout_user()
     flash("You have been logged out.")
 
-    return redirect(url_for('login'))
+    return redirect(url_for('auth.login'))
