d496e4e9db6f7c534810dcb9f7c5b0f1600b409a
ManyLetters1337 <VS20132013@gmail.com>
2019-11-29 16:08:05 +0300

    Refactor code

--- static/plugins/get_data_for_chart.js
+++ static/plugins/get_data_for_chart.js
@@ -3,9 +3,42 @@ String.prototype.replaceAll = function(search, replacement) {
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-$(function () {
+function get_data_for_chart(request_data){
 
-    let COLORS = {
+    request_data = request_data.replace(/^[a-zA-Z0-9]/g, '');
+    request_data = request_data.replace(/[\s\{'"']/g, '');
+    request_data = request_data.split(',').join().split(':').join().split(',')
+
+    let closed = request_data[request_data.indexOf('Closed')+1];
+    let in_progress = request_data[request_data.indexOf('InProgress')+1];
+    let open = request_data[request_data.indexOf('Open')+1];
+    let resolved = request_data[request_data.indexOf('Resolved')+1];
+
+    let chart_data = {
+        'Open' : open,
+        'Closed' : closed,
+        'In Progress' : in_progress,
+        'Resolved' : resolved,
+    }
+
+    return chart_data
+}
+
+function get_data_from_request(){
+    url = 'http://' + window.location.host + '/api' + window.location.pathname;
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, false);
+    xhr.send();
+
+    let uuid_index = xhr.responseText.indexOf('uuid') - 10;
+    let request_data = xhr.responseText.substring(0, uuid_index)
+
+    return request_data
+}
+
+$(function () {
+    const COLORS = {
         'Open' : '#ff4f4f',
         'Closed' : '#a39d9d',
         'In Progress' : '#55b5fa',
@@ -13,25 +46,8 @@ $(function () {
 
     }
 
-    url = 'http://' + window.location.host + '/api' + window.location.pathname;
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('GET', url, false);
-    xhr.send();
-
-
-    let second_ind = xhr.responseText.indexOf('uuid') - 10;
-    let substring = xhr.responseText.substring(0, second_ind)
-    substring = substring.replace(/\s/g, '');
-    substring = substring.replaceAll('{', '');
-    substring = substring.replace('[', '');
-    substring = substring.replaceAll('"', '');
-    substring = substring.split(',').join().split(':').join().split(',')
-
-    let closed = substring[substring.indexOf('Closed')+1];
-    let in_progress = substring[substring.indexOf('InProgress')+1];
-    let open = substring[substring.indexOf('Open')+1];
-    let resolved = substring[substring.indexOf('Resolved')+1];
+    let request_data = get_data_from_request();
+    let chart_data = get_data_for_chart(request_data);
 
     var donutChartCanvas = $('#donutChart').get(0).getContext('2d');
     var donutData  = {
@@ -43,17 +59,16 @@ $(function () {
     ],
     datasets: [
         {
-      data: [open,in_progress,resolved,closed],
+      data: [chart_data['Open'], chart_data['In Progress'], chart_data['Resolved'], chart_data['Closed']],
       backgroundColor : [COLORS['Open'], COLORS['In Progress'], COLORS['Resolved'], COLORS['Closed']],
         }
     ]
     };
-    var donutOptions     = {
+    var donutOptions = {
         maintainAspectRatio : false,
         responsive : true,
     };
-//Create pie or douhnut chart
-// You can switch between pie and douhnut using the method below.
+
     var donutChart = new Chart(donutChartCanvas, {
         type: 'doughnut',
         data: donutData,
