34e2a12871c75cb4b2cb8ec05e5e38e9a86884b0
ManyLetters1337 <VS20132013@gmail.com>
2019-12-03 08:25:37 +0300

    Create Utils Class

---
+++ static/plugins/Utils.js
@@ -0,0 +1,24 @@
+import {ViewChart} from './Chart.js';
+
+class UtilsClass {
+  makeRequest(method, url, callback) {
+     let xhr = new XMLHttpRequest();
+
+     xhr.open(method, url, false);
+     try {
+        xhr.send();
+        if (xhr.status != 200) {
+          alert(`Error ${xhr.status}: ${xhr.statusText}`);
+            }
+        else {
+            callback(JSON.parse(xhr.responseText));
+            }
+        } catch(err) {
+            alert("Request failed");
+        }
+    }
+}
+
+let Utils = new UtilsClass();
+
+export {Utils};
