cc3d16fce981854a5992770c518341f9d97b5cc9
ManyLetters1337 <VS20132013@gmail.com>
2019-12-03 08:25:53 +0300

    Create View

---
+++ static/plugins/View.js
@@ -0,0 +1,18 @@
+import {Utils} from './Utils.js';
+import {ViewChart} from './Chart.js';
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+	
+	Utils.makeRequest('GET', 'http://' + window.location.host + '/api' + window.location.pathname, function(data) {
+	    let result = [];
+	    let project;
+
+	    for (let key in data){
+	    	project = key;
+	    }
+
+	    let chart = new ViewChart(data[project]);
+	    
+	    chart.create_chart();
+	});
+});
