90ba19556e0d5c3e9302e3bf9485220737088ae1
ManyLetters1337 <VS20132013@gmail.com>
2019-12-03 08:25:24 +0300

    Add Chart

---
+++ static/plugins/Chart.js
@@ -0,0 +1,67 @@
+const COLORS = {
+    Open : '#ff4f4f',
+    Closed : '#a39d9d',
+    InProgress: '#55b5fa',
+    Resolved : '#6bfa55'
+};
+
+const STATUSES = {
+	Open : 'Open',
+	Closed : 'Closed',
+	InProgress : 'In Progress',
+	Resolved : 'Resolved'
+};
+
+export class ViewChart{
+	constructor(statuses){
+		this.status_data = statuses;
+	};
+
+	get_canvas(){
+
+		return document.getElementById('donutChart').getContext('2d');
+	};
+
+	set_params(){
+		let donutData = {
+	    labels: [
+	      STATUSES.Open,
+	      STATUSES.InProgress,
+	      STATUSES.Resolved,
+	      STATUSES.Closed,
+	    ],
+	    datasets: [
+	        {
+	      data: [this.status_data[STATUSES.Open], 
+	      		 this.status_data[STATUSES.InProgress],
+	      		 this.status_data[STATUSES.Resolved],
+	      		 this.status_data[STATUSES.Closed]
+	      		],
+	      backgroundColor : [COLORS.Open, COLORS.InProgress, COLORS.Resolved, COLORS.Closed],
+	        }
+	    ]
+	    };
+
+	    return donutData;
+	}
+
+	create_chart(){
+	  
+	    let donutChartCanvas = this.get_canvas();
+	    
+	    let donutData  = this.set_params();
+
+	    let donutOptions = {
+	        maintainAspectRatio : false,
+	        responsive : true,
+	    };
+
+	    let donutChart = new Chart(donutChartCanvas, {
+	        type: 'doughnut',
+	        data: donutData,
+	        options: donutOptions
+	    });
+	}
+}
+
+
