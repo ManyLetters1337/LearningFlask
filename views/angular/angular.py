3b7ffed643e3b0e88008a4efc9bc7bde97ec6457
v.schastny <v.schastny@itransition.com>
2019-12-13 10:22:36 +0300

    Add View Project Statistics

--- views/angular/angular.py
+++ views/angular/angular.py
@@ -1,11 +1,12 @@
 from flask import url_for, render_template, redirect, session, request, Blueprint
+from flask_login import login_required
 
 angular = Blueprint('angular', __name__, template_folder='templates')
 
 
 @angular.route('/', methods=['GET'])
+@login_required
 def angular_main_page():
     return render_template("/angular_main_page.html")
-
 
 
